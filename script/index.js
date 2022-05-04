399eda7f424c0556940c0171eae2d6f304f463d0
Alex Sarva <alexsarva@yandex.ru>
2022-05-04 16:46:01 +0300

    fix: Исправить проект по замечаниям

--- script/index.js
+++ script/index.js
@@ -2,42 +2,36 @@ const qs = (selector) => document.querySelector(selector);
 
 const formElement = qs('.popup'); // форма
 
-const nameInput = qs('.popup__item_type_name'); // Поле ввода имени
-const jobInput = qs('.popup__item_type_job'); // Поле ввода работы
+const nameInput = qs('.popup__field_type_name'); // Поле ввода имени
+const jobInput = qs('.popup__field_type_job'); // Поле ввода работы
 
 
 const editBtn = qs('.profile__edit-btn'); // Кнопка редактирования профиля
 const closeBtn = qs('.popup__close-btn'); // Кнопка закрытия попапа
 const saveBtn = qs('.popup__save-btn'); // Кнопка сохранения попапа
 
-const existName = qs('.profile__name'); // Имя в профиле
-const existJob = qs('.profile__occupation'); // Работа в профиле
+const profileName = qs('.profile__name'); // Имя в профиле
+const profileJob = qs('.profile__occupation'); // Работа в профиле
 
 
 function togglePopup(popupObject) {
     popupObject.classList.toggle('popup_opened');
 }
 
-function editProfile(evt) {
-    console.log('editBtn');
-    evt.preventDefault();
+function editProfile() {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
     togglePopup(formElement);
-    nameInput.value = existName.textContent;
-    jobInput.value = existJob.textContent;
 }
 
 function saveProfile(evt) {
-    console.log('saveBtn');
     evt.preventDefault();
-    console.log(nameInput.value, jobInput.value);
-    existName.textContent = nameInput.value;
-    existJob.textContent = jobInput.value;
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
     togglePopup(formElement);
 }
 
-function closePopup(evt) {
-    console.log('closeBtn');
-    console.log(evt);
+function closePopup() {
     togglePopup(formElement);
 }
 
